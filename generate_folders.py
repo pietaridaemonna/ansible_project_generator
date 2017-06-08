f97c4006f3b0137b2d2fb83e56c133f4b9178c10
peter.ducai <peter.ducai@gmail.com>
2017-06-08 13:01:08 +0200

    cleanup

--- generate_folders.py
+++ generate_folders.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 #  ___            _ _     _        _____                           _             
 # / _ \          (_) |   | |      |  __ \                         | |            
 #/ /_\ \_ __  ___ _| |__ | | ___  | |  \/ ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
@@ -10,59 +12,43 @@ import os
 import re
 from pathlib import Path 
 
+current = os.getcwd()
+os.chdir( current )
 
 
 # Get user's project_name
-project_name = raw_input("Please enter project_name of project: ")
+project_name = input("Please enter project_name of project: ")
 
 # While project_name has incorrect characters
-while re.search('[^a-zA-Z\n]',project_name):
+while re.search('[^a-zA-Z\d\_\n]',project_name):
 
     # Print out an error
     print("illegal project_name - Please use only letters")
 
     # Ask for the project_name again (if it's incorrect, while loop starts again)
-    project_name = raw_input("Please enter project_name of project: ")
+    project_name = input("Please enter project_name of project: ")
+
+os.makedirs(project_name)
+os.chdir(project_name)
     
 
 # Get user's inventory_type
-inventory_type = raw_input("Please enter inventory_type [static/dynamic]: ")
+inventory_type = input("Please enter inventory_type [static/dynamic]: ")
 
 # While inventory_type has incorrect characters
-while re.search('[^a-zA-Z\n]',inventory_type):
+while re.search('[^\bstatic\b | \bdynamic\b\n]',inventory_type):
 
     # Print out an error
-    print("illegal inventory_type - Please use only letters")
+    print("illegal inventory_type - Please use only static OR dynamic")
 
     # Ask for the inventory_type again (if it's incorrect, while loop starts again)
-    inventory_type = raw_input("Please enter inventory_type of project: ")
+    inventory_type = input("Please enter inventory_type of project: ")
 
 
 
 
 
 
-# base directories
-
-path = Path('INVENTORIES')  # we'll have production, staging/test, dev
-path.parent.mkdir(parents=True, exist_ok=True) 
-path = Path('INVENTORIES/production')
-path.parent.mkdir(parents=True, exist_ok=True) 
-path = Path('INVENTORIES/test')
-path.parent.mkdir(parents=True, exist_ok=True) 
-path = Path('INVENTORIES/dev')
-path.parent.mkdir(parents=True, exist_ok=True) 
-
-path = Path('LIBRARY')      # if any custom modules, put them here (optional)
-path.parent.mkdir(parents=True, exist_ok=True) 
-
-path = Path('ROLES')
-path.parent.mkdir(parents=True, exist_ok=True) 
-path = Path('ROLES')
-path.parent.mkdir(parents=True, exist_ok=True) 
-path = Path('ROLES')
-path.parent.mkdir(parents=True, exist_ok=True) 
-#Path('ROLES/').touch()
 
 
 # FUNCTIONS ---------------------------------------------------------------------
